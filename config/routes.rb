bcbc630f7b0e3e7474d342d91c0cd072fc4984cd
Rinat273 <bairam0487@gmail.com>
2024-04-29 18:39:02 +0400

    delete api trains and workouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :trains do
-        resources :workouts
-      end
-    end
-  end
+
 
   resources :workouts
   resources :trains

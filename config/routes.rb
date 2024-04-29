036da2494884cba56167c7788d0be05202a0caba
Rinat273 <bairam0487@gmail.com>
2024-04-29 18:30:27 +0400

    add api trains and workouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :trains do
+        resources :workouts
+      end
+    end
+  end
+
   resources :workouts
   resources :trains
   resources :exercises
